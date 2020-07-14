d90b63000ee607313d8695ce2ff86fa85e1deb52
ParkSang <hooo0503@gmail.com>
2020-07-14 23:01:59 +0900

    jsx_and_props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTyes from "prop-types";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React, Very Awesome Guy</h1>
     </div>
   );
 }
