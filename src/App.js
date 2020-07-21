2e9f1948f5d59c50b4afe3f96dc790ca70a350bd
ParkSang <hooo0503@gmail.com>
2020-07-21 17:11:37 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
-import PropTyes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie.js";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React, Very Awesome Guy</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => {
+              return (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
